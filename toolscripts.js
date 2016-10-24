e1f19f5dd2d7c2835200d2a8992bc6da9f383bf9
Namjoon Joo <Namjoon Joo>
2016-10-23 22:19:55 -0700

    Added client-sided storage functionality up to adding and removing from the list (without refreshes or closing the browser).

--- toolscripts.js
+++ toolscripts.js
@@ -1,33 +1,44 @@
-$.fn.result = function ()
-{
+$.fn.result = function () {
    var input = document.getElementById('searchInput').value;
-   var url = "https://www.ifixit.com/api/2.0/suggest/" + input + "?doctypes=category";
-   $.getJSON(url, function (data)
-   {
+   var url = "https://www.ifixit.com/api/2.0/suggest/" + input + "?doctypes=device";
+   $.getJSON(url, function (data) {
       search(data);
    });
 };
 
-$(document).ready(function ()
-{
-   /* TODO fill list with existing data from client-sided storage */
+$(document).ready(function () {
+   if (!localStorage.devices) {
+      localStorage.devices = JSON.stringify([]);
+   }
 
-   if (!navigator.onLine)
-   {
+   /* fill list with existing data from client-sided storage */
+   /*
+   var devices = JSON.parse(localStorage["devices"]);
+   console.log(devices);
+   for (var i = 0; i < devices.length; i++) {
+      if (devices[i].url == "") {
+         addUnlinkedItem(devices[i].name);
+      }
+      else {
+         addLinkedItem(devices[i].name, devices[i].url);
+      }
+   }
+   */
+
+
+
+   if (!navigator.onLine) {
       var input = document.getElementById("searchInput");
       input.setAttribute("placeholder", "No connection. Press [ENTER] to add device...");
    }
 
-   $(window).keydown(function (event)
-   {
-      if (event.keyCode == 13)
-      {
+   $(window).keydown(function (event) {
+      if (event.keyCode == 13) {
          var input = document.getElementById("searchInput");
          var pattern = new RegExp("^\\s*$");
          event.preventDefault();
-         if (!pattern.test(input.value))
-         {
-            addUnlinked();
+         if (!pattern.test(input.value)) {
+            addUnlinkedItem(document.getElementById('searchInput').value);
          }
          var input = document.getElementById("searchInput");
          input.value = "";
@@ -36,21 +47,23 @@ $(document).ready(function ()
    });
 });
 
-function search(li)
-{
-   var ul, i, link, elem;
+$(document).ready(function () {
+   $('#clearButton').click(function () {
+      clear();
+   });
+});
+
+function search(li) {
+   var ul, link, elem;
    ul = document.getElementById("searchList");
    elem = document.getElementById("searchItem");
-   while (elem != null)
-   {
+   while (elem != null) {
       elem.remove();
       elem = document.getElementById("searchItem");
    }
 
-   for (i = 0; i < li.results.length; i++)
-   {
-      (function(index)
-      {
+   for (var i = 0; i < li.results.length; i++) {
+      (function (index) {
          var entry, span, div, a;
          link = li.results[index].url;
          entry = document.createElement("li");
@@ -61,9 +74,8 @@ function search(li)
          div = document.createElement("div");
          div.setAttribute("class", "addButton");
          a = document.createElement("a");
-         a.onclick = function()
-         {
-            addLinked(li.results[index]);
+         a.onclick = function () {
+            addLinkedItem(li.results[index].display_title, li.results[index].url);
          }
          div.appendChild(a);
          span.appendChild(div);
@@ -73,66 +85,80 @@ function search(li)
    }
 }
 
-function addLinked(device)
-{
-   var ul, entry, span, div, a;
-   entry = document.createElement("li");
-   entry.setAttribute("class", "listItem");
-   entry.setAttribute("id", "deviceItem");
-   span = document.createElement("span");
-   span.innerHTML = "<div class='linkButton'><a href=" + device.url + "></a></div>"
-      + device.display_title;
-   div = document.createElement("div");
-   div.setAttribute("class", "removeButton");
-   a = document.createElement("a");
-   a.onclick = function()
-   {
-      removeLinkedItem(entry);
+function addUnlinkedItem(name) {
+   if (localStorage.getItem(name) != null) {
+      alert("You already own a(n) " + name + ".");
    }
-   div.appendChild(a);
-   span.appendChild(div);
-   entry.appendChild(span);
-   ul = document.getElementById("deviceList");
-   ul.appendChild(entry);
+   else {
+      var ul, entry, span, div, a;
+      entry = document.createElement("li");
+      entry.setAttribute("class", "listItem");
+      entry.setAttribute("id", "deviceItem");
+      span = document.createElement("span");
+      span.innerHTML = name;
+      div = document.createElement("div");
+      div.setAttribute("class", "removeButton");
+      a = document.createElement("a");
+      a.onclick = function () {
+         removeItem(entry, name);
+      }
+      div.appendChild(a);
+      span.appendChild(div);
+      entry.appendChild(span);
+      ul = document.getElementById("deviceList");
+      ul.appendChild(entry);
 
-   /* TODO add to client-sided storage */
+      /* add to client-sided storage */
+      var memEntry = { name: name, url: "" };
+      localStorage.setItem(name, memEntry);
+   }
 }
 
-function addUnlinked()
-{
-   var ul, name, entry, span, div, a;
-   name = document.getElementById('searchInput').value;
-   entry = document.createElement("li");
-   entry.setAttribute("class", "listItem");
-   entry.setAttribute("id", "deviceItem");
-   span = document.createElement("span");
-   span.innerHTML = name;
-   div = document.createElement("div");
-   div.setAttribute("class", "removeButton");
-   a = document.createElement("a");
-   a.onclick = function () {
-      removeUnlinkedItem(entry);
+function addLinkedItem(name, url) {
+   if (localStorage.getItem(name) != null) {
+      alert("You already own a(n) " + name + ".");
    }
-   div.appendChild(a);
-   span.appendChild(div);
-   entry.appendChild(span);
-   ul = document.getElementById("deviceList");
-   ul.appendChild(entry);
+   else {
+      var ul, entry, span, div, a;
+      entry = document.createElement("li");
+      entry.setAttribute("class", "listItem");
+      entry.setAttribute("id", "deviceItem");
+      span = document.createElement("span");
+      span.innerHTML = "<div class='linkButton'><a href=" + url + "></a></div>"
+         + name;
+      div = document.createElement("div");
+      div.setAttribute("class", "removeButton");
+      a = document.createElement("a");
+      a.onclick = function () {
+         removeItem(entry, name);
+      }
+      div.appendChild(a);
+      span.appendChild(div);
+      entry.appendChild(span);
+      ul = document.getElementById("deviceList");
+      ul.appendChild(entry);
 
-   /* TODO add to client-sided storage */
+      /* add to client-sided storage */
+      var memEntry = { name: name, url: url };
+      localStorage.setItem(name, memEntry);
+   }
 }
 
-function removeLinkedItem(elem)
-{
+function removeItem(elem, name) {
    elem.remove();
 
-   /* TODO remove from client-sided storage */
+   /* remove from client-sided storage */
+   localStorage.removeItem(name);
 }
 
-/* TODO check if unlinked version is necessary */
-function removeUnlinkedItem(elem)
+function clear()
 {
-   elem.remove();
-
-   /* TODO remove from client-sided storage */
+   localStorage.clear();
+   ul = document.getElementById("deviceList");
+   elem = document.getElementById("deviceItem");
+   while (elem != null) {
+      elem.remove();
+      elem = document.getElementById("deviceItem");
+   }
+   alert("Cleared device list!");
 }
